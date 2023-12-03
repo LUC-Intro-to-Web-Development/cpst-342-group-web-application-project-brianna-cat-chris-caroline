46dfd2e363ac839a4355ac69484c6067d9bb6622
Chouser519 <148822156+Chouser519@users.noreply.github.com>
2023-12-03 11:14:17 -0600

    commented out pre-existing code

--- index.js
+++ index.js
@@ -14,26 +14,27 @@ app.engine('html', require('hbs').__express);
  app.use(express.urlencoded({ extended: true }));
 
 
-function displayAge(userAge) {
-    console.log('I am ' + userAge + ' years old.');
- }
+// function displayAge(userAge) {
+//     console.log('I am ' + userAge + ' years old.');
+//  }
 
-displayAge(34)
+// displayAge(34)
 
- function displaySub(num1, num2) {
-    console.log(num1 - num2)
- }
+//  function displaySub(num1, num2) {
+//     console.log(num1 - num2)
+//  }
 
- displaySub(18, 6)
+//  displaySub(18, 6)
 
- let madLib = function(adverb, verb, noun1, noun2, adj) {
-    return ("The " + adj + " " + noun1 + " " + adverb + " " + verb + " the " + noun2 + ".");
- }
+//  let madLib = function(adverb, verb, noun1, noun2, adj) {
+//     return ("The " + adj + " " + noun1 + " " + adverb + " " + verb + " the " + noun2 + ".");
+//  }
 
- console.log(madLib("quickly", "chased", "dog", "cat", "large"))
+//  console.log(madLib("quickly", "chased", "dog", "cat", "large"))
 
- function displayAdd (num1, num2) {
-    console.log(num1 + num2)
- }
+//  function displayAdd (num1, num2) {
+//     console.log(num1 + num2)
+//  }
 
- displayAdd(18, 6)
+//  displayAdd(18, 6)
+
